2d8c926e397dc86e1ab75b6a43ad9a0692223618
Francisco Knebel <KyoZeus@users.noreply.github.com>
2016-06-07 18:15:08 -0300

    Adicionado testes.

---
+++ source/src/tests/TestConference.java
@@ -0,0 +1,99 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestConference {
+
+	@Test
+	public void testConference() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAcronym() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAllocate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAcceptedPapers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRejectedPapers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAreAllPapersReviewed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ source/src/tests/TestResearcher.java
@@ -0,0 +1,72 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import domain.Paper;
+import domain.Researcher;
+
+public class TestResearcher {
+	Researcher author, researcher, researcher2, researcher3;
+	Paper paper;
+	List<String> researchTopics;
+
+	@Before
+	public void setUp() throws Exception {
+		researchTopics = new ArrayList<String>();
+		researchTopics.add("Nadica");	
+		researchTopics.add("Nada");	
+		researcher = new Researcher(0, "Bob", "UFRGS2", researchTopics);
+		
+		researchTopics = new ArrayList<String>();
+		researchTopics.add("Nada");	
+		researchTopics.add("Nadica");	
+		researcher2 = new Researcher(1, "BoboB", "UFRGS", researchTopics);
+		
+		researchTopics = new ArrayList<String>();
+		researchTopics.add("Nado");	
+		researchTopics.add("Nadica");	
+		researcher3 = new Researcher(1, "Master of puppets", "UFRGS2", researchTopics);
+		
+		researchTopics = new ArrayList<String>();
+		researchTopics.add("Nada");	
+		researchTopics.add("Nado");	
+		author = new Researcher(1, "Dion Dou", "UFRGS", researchTopics);
+		
+		paper = new Paper(0, "Paper de Teste", author, "ABC", "Nada");
+	}
+	
+	@Test
+	public void testIncrementNumberOfReviews() {
+		assertTrue(researcher.getNumberOfReviews() == 0);
+		researcher.incrementNumberOfReviews();
+		assertTrue(researcher.getNumberOfReviews() == 1);
+	}
+
+	@Test
+	public void testDecrementNumberOfReviews() {
+		assertTrue(researcher.getNumberOfReviews() == 0);
+		researcher.decrementNumberOfReviews();
+		assertTrue(researcher.getNumberOfReviews() == -1);
+	}
+
+	@Test
+	public void testCheckCandidate() {
+		//universidades diferentes  && possui o topico && ainda nao alocado
+		assertTrue(researcher.checkCandidate(paper));
+		
+		//autor e revisor da mesma univesidade
+		assertFalse(researcher2.checkCandidate(paper));
+		//revisor nao tem o topico de pesquisa do paper
+		assertFalse(researcher3.checkCandidate(paper));
+		//revisor ja alocado para esse paper
+		paper.addReviewer(researcher);
+		assertFalse(researcher.checkCandidate(paper));
+		
+	}
+}
