587abfc600702e43aecf98be94a77bc0fc636a92
leonardodalcin <leonardodalcin@outlook.com>
2016-06-07 18:21:01 -0300

    inicio da implementação de execute

--- source/src/Interface/ConferenceAllocateCommand.java
+++ source/src/Interface/ConferenceAllocateCommand.java
@@ -1,27 +1,64 @@
 package Interface;
 
 import Interface.ui.text.UIUtils;
+import domain.Conference;
 import domain.Database;
+import domain.Review;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Scanner;
+
 
 public class ConferenceAllocateCommand extends AbstractChairHelperCommand {
 	
 	private Database database;
 	private UIUtils uiUtils;
-
+	private Scanner scanner;
+	
+	private int selectedConferenceId;
+	private Conference selectedConference;
+	
+	private int numReviewers;
+	
+	private Collection<Conference> conferencesList;
+	
 	public ConferenceAllocateCommand(UIUtils uiUtils, Database database){
 		this.database = database;
 		this.uiUtils = uiUtils;
 	}
 	
 	private void askConference(){
-		
+		System.out.println(": ");
+		scanner = new Scanner(System.in);
+		selectedConferenceId = scanner.nextInt(); // ask reviewer
+		scanner.close();
 	}
 	
 	private void askNumReviewers(){
-		
+		System.out.println(": ");
+		scanner = new Scanner(System.in);
+		numReviewers = scanner.nextInt(); // ask reviewer
+		scanner.close();
 	}
 	
-	public void execute(){
+	public void execute() {
+		this.conferencesList = database.getAllConferences();
+		askConference();
+		selectedConference = searchConference( selectedConferenceId );
+		askNumReviewers();
+		selectedConference.allocate(numReviewers);
 		
+		
+
+	}
+	
+	private Conference searchConference( int conferenceId ) {
+		for ( Conference conference : this.conferencesList) {
+			if( conference.getId() == conferenceId ) {
+				return conference;
+			}
+		}
+		return null;
 	}
 }
